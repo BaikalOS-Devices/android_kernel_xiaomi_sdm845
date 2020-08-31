95edc54cda624dae42e18a0b5458fcdf1e06a008
Sergey Vasiliev <sdv22@live.ru>
2020-08-31 11:53:54 +0300

    Fix build after cherrry-pick

--- drivers/input/fingerprint/goodix_ta/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta/gf_spi.c
@@ -720,8 +720,9 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 	unsigned int blank;
 	char temp[4] = { 0x0 };
 
-	if (val != DRM_EVENT_BLANK)
+	if (val != MSM_DRM_EARLY_EVENT_BLANK)
 		return 0;
+
 	pr_debug("[info] %s go to the goodix_fb_state_chg_callback value = %d\n",
 			__func__, (int)val);
 	gf_dev = container_of(nb, struct gf_dev, notifier);
