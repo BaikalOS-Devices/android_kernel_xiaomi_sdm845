49581e8d40519b0b8eb18bd01a549f9a991c58a4
Sergey Vasiliev <sdv22@live.ru>
2020-09-04 21:36:50 +0300

    3sec boost on app start

--- kernel/fork.c
+++ kernel/fork.c
@@ -80,6 +80,7 @@
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
+#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2109,6 +2110,10 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+    /* Boost CPU to the max for 3000 ms when userspace launches an app */
+	if (task_is_zygote(current))
+		cpu_input_boost_kick_max(3000);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
